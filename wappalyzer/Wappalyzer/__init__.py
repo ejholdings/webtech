2134e5a8a55ae19707d80d37f7799150d4151a77
Ahmed Hassan <ahmed.89.hassan@gmail.com>
2023-09-15 14:01:25 +0300

    u

---
+++ wappalyzer/Wappalyzer/__init__.py
@@ -0,0 +1,10 @@
+"""
+Welcome to ``python-Wappalyzer`` API documentation!
+
+:see: `Wappalyzer` and `WebPage`.
+"""
+
+from .Wappalyzer import Wappalyzer, analyze
+from .webpage import WebPage
+
+__all__ = ["Wappalyzer", "WebPage", "analyze"]
